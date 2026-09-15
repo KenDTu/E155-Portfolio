5c7af3ba570d5f7aca5dd5623d6d44cbd8c412fb
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-15 16:18:08 -0700

    Add enable input to counter module
    Added enable input to counter module and modified logic.

--- labs/lab1/counter.sv
+++ labs/lab1/counter.sv
@@ -6,6 +6,7 @@
 module counter #(parameter N = 8, parameter max = 24000000)
 				 (input logic      int_osc,
 				  input logic      reset,
+				  input logic      enable,
 			      output logic     ledLast
 );
 	logic [24:0] counter;
@@ -14,6 +15,7 @@ module counter #(parameter N = 8, parameter max = 24000000)
 		begin
 			if(reset == 0)                counter <= 0;
 			else if (counter == max)      counter <= 0;
+			// else if (enable == 1) counter <= counter + 1;
 			else                counter <= counter + 1;
 		end
 
