075ebfbc3bb071a2c9cb84167f9252dc8a9c8d70
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-16 15:57:26 -0700

    Modify counter module to include enable functionality
    Added enable condition to counter logic.

--- labs/lab1/counter.sv
+++ labs/lab1/counter.sv
@@ -1,21 +1,21 @@
 // Author: Ken TU
 // Email: ktu@g.hmc.edu
 // Date of Creation: 09/07/2026
-// Counter module for the 2.4Hz flashing LED
+// Counter module for the 2.4Hz flashing LED, max = 24000000
 
-module counter #(parameter N = 24, parameter max = 24000000)
+module counter #(parameter counterWidth = 24, parameter max = 24000000)
 				 (input logic      int_osc,
 				  input logic      reset,
 				  input logic      enable,
 			      output logic     ledLast
 );
-	logic [counterWidth:0] counter;
+	logic [counterWidth-1:0] counter;
 
 	always_ff @(posedge int_osc, negedge reset)  // TODO: use the enable and verify your logic
 		begin
 			if(reset == 0)                counter <= 0;
 			else if (counter == max)      counter <= 0;
-			else                counter <= counter + 1;
+			else if (enable == 1)         counter <= counter + 1;
 		end
 
 
