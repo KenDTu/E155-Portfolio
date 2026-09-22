f36b3f79157c23952f71fdcd81ddd4ce8e445fb1
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-21 18:54:53 -0700

    Update counter module parameters and logic

--- labs/lab2/counter.sv
+++ labs/lab2/counter.sv
@@ -5,14 +5,12 @@
 
 // counterWidth = 2^19 = 524,288 ≥ 400,000
 // clock math is 24MHz / 400,000 = 60Hz
-module counter #(parameter counterWidth = 19, parameter max = 399,999) // TODO: adjust the counterWidth and max to have correct clock divider
-				 (input logic      int_osc,
-				  input logic      reset,
-				  input logic      enable,
-				  output logic     leftHex,
-			      output logic     ledLast
+module counter #(parameter counterWidth = 20, parameter max = 499999) // TODO: adjust the counterWidth and max to have correct clock divider
+				 (input logic       int_osc,
+				  input logic       reset,
+				  input logic       enable,
+				  output logic [counterWidth-1:0] counter
 );
-	logic [counterWidth-1:0] counter;
 
 	always_ff @(posedge int_osc, negedge reset)  // TODO: use the enable and verify your logic
 		begin
@@ -20,12 +18,4 @@ module counter #(parameter counterWidth = 19, parameter max = 399,999) // TODO: 
 			else if (counter == max)      counter <= 0;
 			else if (enable == 1)         counter <= counter + 1;
 		end
-
-
-	// output of the last led light from the counter
-	assign ledLast = counter[23];
-
-	// output of the leftHex to be on or off using logical assignment
-	assign leftHex = (count > max/2);
-
 endmodule
