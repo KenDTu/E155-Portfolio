00da4d0284a8fd8ab9e884e6f018a0976f218fd5
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-21 20:18:39 -0700

    Refactor lab2_ktu module with updated assignments
    Updated pin assignments and logic for leftHex and led outputs.

--- labs/lab2/lab2_ktu.sv
+++ labs/lab2/lab2_ktu.sv
@@ -42,7 +42,8 @@ module lab2_ktu #(parameter max = 499999)
 	counter counter1(.int_osc(int_osc), .reset(reset), .enable(enable), .counter(counter));
 	sevenseg sevensegData(.s(s), .segments(seg));
 	
-	assign leftHex = (counter > max/2);
+	// assign leftHex = (counter > max/2);
+	assign leftHex = counter[19];
 	assign rightHex = ~leftHex; // if rightHex is on then leftHex should be off
 	
 	assign s = leftHex ? switchLeft : switchRight;
@@ -53,5 +54,5 @@ module lab2_ktu #(parameter max = 499999)
     //  Assign LED output
 	assign led[0] = switchLeft[1] ^ switchLeft[0];
 	assign led[1] = switchLeft[3] && switchLeft[2];
-	assign led[2] = counter[19];
+	assign led[2] = counter[0];
 endmodule
