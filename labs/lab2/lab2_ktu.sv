8bafb056182c0675fa64543536f4b869c5bcb66c
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-21 18:54:35 -0700

    Refactor lab2_ktu module with parameter and outputs
    Updated the lab2_ktu module to include a parameter for max count and added outputs for leftHex and rightHex. Adjusted internal logic and instantiation of the counter.

--- labs/lab2/lab2_ktu.sv
+++ labs/lab2/lab2_ktu.sv
@@ -20,33 +20,32 @@ seg[6], seg[5], seg[4], seg[3], seg[2], seg[1], seg[0]
 see iPad
 */
 
-module lab2_ktu(
-		input logic [3:0] switchLeft, // the four DIP switches (on the board, SW6)
+module lab2_ktu #(parameter max = 499999)
+	   (input logic [3:0] switchLeft, // the four DIP switches (on the board, SW6)
 		input logic [3:0] switchRight, // the off board DIP switch
 		input logic reset, // switch for reset on the board
 		input logic enable, // switch for enable on the board
 		output logic [2:0] led,   // 3 LEDs (you may use the on-board LEDs)
-		output logic [6:0] seg   // the segments of a common-anode 7-segment display
+		output logic [6:0] seg,   // the segments of a common-anode 7-segment display
+		output leftHex, // to toggle the left seven segment power
+		output rightHex // to toggle the right seven segment power
 );
-
+	logic [3:0] s;
 	logic int_osc;
-	logic leftHex; // to toggle the left seven segment power
-	logic rightHex; // to toggle the right seven segment power
-	
+	logic [19:0] counter;
+
 	// Internal high-speed oscillator
 	HSOSC #(.CLKHF_DIV(2'b01))
 		hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
-
-   
-	// Instantiating Seven Segement displayer decoder
-	sevenseg sevensegData(.s(s), .segments(seg));
-
-	// Instantiating Counter to oscillate the l
-	counter counter1(.int_osc(int_osc), .reset(reset), .enable(enable), .leftHex(.leftHex), .ledLast(led[2]));
 	
+	// Instantiating Counter to oscillate the l
+	counter counter1(.int_osc(int_osc), .reset(reset), .enable(enable), .counter(counter));
+	sevenseg sevensegData(.s(s), .segments(seg));
+	
+	assign leftHex = (counter > max/2);
 	assign rightHex = ~leftHex; // if rightHex is on then leftHex should be off
 	
-	// TODO: pin assignments according to lab 2.
+	assign s = leftHex ? switchLeft : switchRight;
 	
 	// switchRight[3] - P4, switchRight[2] - P48, switchRight[1] - P47, switchRight[0] - P2
 	
@@ -54,6 +53,5 @@ module lab2_ktu(
     //  Assign LED output
 	assign led[0] = switchLeft[1] ^ switchLeft[0];
 	assign led[1] = switchLeft[3] && switchLeft[2];
-
-
+	assign led[2] = counter[19];
 endmodule
