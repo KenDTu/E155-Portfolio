b41860c09896974494c9f46dae7847eb912a0c69
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-21 20:44:27 -0700

    Refactor counter output and comments in lab2_ktu.sv
    Updated the counter output assignment and corrected comments.

--- labs/lab2/lab2_ktu.sv
+++ labs/lab2/lab2_ktu.sv
@@ -43,7 +43,7 @@ module lab2_ktu #(parameter max = 499999)
 	sevenseg sevensegData(.s(s), .segments(seg));
 	
 	// assign leftHex = (counter > max/2);
-	assign leftHex = counter[19];
+	assign leftHex = counter[17];
 	assign rightHex = ~leftHex; // if rightHex is on then leftHex should be off
 	
 	assign s = leftHex ? switchLeft : switchRight;
@@ -54,5 +54,5 @@ module lab2_ktu #(parameter max = 499999)
     //  Assign LED output
 	assign led[0] = switchLeft[1] ^ switchLeft[0];
 	assign led[1] = switchLeft[3] && switchLeft[2];
-	assign led[2] = counter[0];
+	assign led[2] = counter[17];
 endmodule
