40cad46dc67094d3048fefe99c4a4bfe1f83b4f5
Ken Tu <65514405+KenDTu@users.noreply.github.com>
2026-09-22 21:40:46 -0700

    Refactor counter module to use clk instead of int_osc

--- labs/lab2/counter.sv
+++ labs/lab2/counter.sv
@@ -6,13 +6,13 @@
 // counterWidth = 2^19 = 524,288 ≥ 400,000
 // clock math is 24MHz / 400,000 = 60Hz
 module counter #(parameter counterWidth = 18, parameter max = 249999) // TODO: adjust the counterWidth and max to have correct clock divider
-				 (input logic       int_osc,
+				 (input logic       clk,
 				  input logic       reset,
 				  input logic       enable,
 				  output logic [counterWidth-1:0] counter
 );
 
-	always_ff @(posedge int_osc, negedge reset)  // TODO: use the enable and verify your logic
+	always_ff @(posedge clk, negedge reset)
 		begin
 			if(reset == 0)                counter <= 0;
 			else if (counter == max)      counter <= 0;
